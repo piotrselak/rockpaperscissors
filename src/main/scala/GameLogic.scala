77ffd270e666a41cb5fe5a66ec50083d9f1ba3bd
piotr.selak <p.selak.848@studms.ug.edu.pl>
2022-06-22 14:09:40 +0200

    Function playerChoice added, which parses user input

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -1,5 +1,25 @@
 package gamelogic
 
-object Compare {
-  
+import zio._
+
+import weapons._
+
+import exceptions.WrongInputException
+
+object Logic {
+
+  def playerChoice(input: String): IO[WrongInputException, Weapon] = {
+    val lowerCaseInput = input.toLowerCase
+    lowerCaseInput match {
+      case "r" => ZIO.succeed(Rock)
+      case "p" => ZIO.succeed(Paper)
+      case "s" => ZIO.succeed(Scissors)
+      case _ => ZIO.fail(WrongInputException())
+    }
+  }
+
+  def computerChoice(): Task[Weapon] = {
+    
+  }
+
 }
