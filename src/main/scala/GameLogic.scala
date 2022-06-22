aab189cd4f140b262e3d572a53d41b784c3b53bc
piotr.selak <p.selak.848@studms.ug.edu.pl>
2022-06-22 15:16:56 +0200

    Added init of RNG class

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -1,9 +1,32 @@
 package gamelogic
 
-import weapons._
+/*
+ * Probably the greatest choice of word in the whole universe
+*/
+sealed trait Weapon
+
+final case object Rock extends Weapon
+final case object Paper extends Weapon
+final case object Scissors extends Weapon
+
+final case class WrongInput(name: String = "Wrong input!") extends Exception
+
+sealed private case class RNG(seed: Long) {
+  def getRand() : (Int, RNG) = {
+    val randomValue = ???
+    val newSeed = ???
+
+    (randomValue, RNG(newSeed))
+  }
+}
 
 object Logic {
+  def parseUserChoice(input: String): Either[WrongInput, Weapon] = input.toLowerCase match {
+    case "r" => Right(Rock)
+    case "p" => Right(Paper)
+    case "s" => Right(Scissors)
+    case _ => Left(WrongInput())
+  }
 
-  
 
 }
